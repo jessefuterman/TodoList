43cf47f6f09e2204fb1ce29a0057612d65c61ced
Jesse Futerman <jessefuterman@gmail.com>
2019-08-22 15:02:45 -0400

    aug22

--- src/App.js
+++ src/App.js
@@ -1,26 +1,159 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Users from "./Users";
+import { Checkbox } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     text: "",
+      selectedOption: "",
+      items: [],
+      pushedUser:"",
+      groupLatinJazz: this.props.passingLatinJazz,
+      users: [
+        "Jesse ",
+        "Richard ",
+        "Vince ",
+        "KoolGuy22 ",
+        "Amanda ",
+        "Revolver Ocelot ",
+        "Snake ",
+        "Greg ",
+        "Ben ",
+        "Matt ",
+        "Tito Puente ",
+        "Braden ",
+        "Adam ",
+        "Alex ",
+        "Nathan "
+      ]
+    };
+  }
+
+
+  
+  
+
+  onChange = event => {
+   
+    
+    console.log(event)
+  };
+
+
+  handleChange = (event) => {
+ 
+   
+   
+     this.onChange()
+     this.setState({ selectedOption: event.target.value });
+     console.log(event.target.value, "what is event target")
+   
+   
+  };
+
+  handleSubmit = (event, elem) => {
+   console.log('is it in handlesubmit')
+   
+   this.setState({ pushedUser: elem })
+   
+   console.log(elem)
+   
+    this.triggerGroup()
+  }
+
+  triggerGroup = () => {
+    if (this.state.selectedOption === "LatinJazzFans") {
+      // this.passingGroup()
+      // this.setState({
+      //   groups: this.state.users[0].push(this.state.groupLatinJazz.LatinJazzFans)
+      // });
+      // console.log(this.state.users)
+      this.LatinJazzFans();
+    }
+
+  
+    if (this.state.selectedOption === "HouseHeads") {
+      this.HouseHeads();
+    }
+
+    if (this.state.selectedOption === "FunkFans") {
+      this.FunkFans();
+    }
+    if (this.state.selectedOption === "JazzCats") {
+      this.JazzCats();
+    }
+  };
+
+ LatinJazzFans = () => {
+  
+console.log("we gettin in latin?")
+
+  
+
+
+
+
+ }
+
+ HouseHeads = () => {
+
+
+ }
+
+ FunkFans = () => {
+
+
+ }
+
+ JazzCats = () => {
+
+
+
+ }
+ 
+ 
+
+
+
+  listLogic = () => {
+    //  let del = this.state.users.filter(x => x !== str);
+    let mapOne = this.state.users.map(elem => (
+      <ul>
+        {elem}
+
+          
+       
+         
+        
+      
+        <button className = "buttonThree" onClick = {(event) => {this.handleSubmit(event,elem)}}>Add to Group</button>
+        <Checkbox label='Add to LatinJazz Fans'   onChange={(event) => {this.handleChange(event,elem)}}  value="LatinJazzFans"  />
+        <Checkbox label='Add to Funk Fans'   onChange={(event) => {this.handleChange(event,elem)}} value="FunkFans"/>
+        <Checkbox label='Add to JazzCats' onChange={(event) => {this.handleChange(event,elem)}}  value = "JazzCats"/>
+       <Checkbox label='Add to House Heads' onChange={(event) => {this.handleChange(event,elem)}}  value = "HouseHeads"/>
+      </ul>
+    ));
+
+    return mapOne;
+  };
+
+  render() {
+    return (
+      <div className="NameList">
+        <div>
+          <Users passingUsers={this.state.users}  passingPushed={this.state.pushedUser} passingOption = {this.state.selectedOption} />
+          <h4 className="App">Music Fans Unite</h4>
+        </div>
+        <div>
+          <ul>{this.listLogic()}</ul>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Users.js
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pushedUser: this.props.passingPushed,
+        users: this.props.passingUsers,
+        userSelectedOption: this.props.passingOption,
+      selectedOption: "",
+      groups: {
+        LatinJazzFans: ["Denny ", "Lenny ", "Penny "],
+        
+        HouseHeads: ["Jarry ", "Larry ", "Kerri "],
+       
+        FunkFans: ["Spiro ", "FonkUpdate ", "Cherry23 "],
+        JazzCats: ["Pharoah ", "Coltrane ", " Monk "]
+      },
+      text: "",
+      items: []
+    };
+  }
+
+  triggerGroup = () => {
+   
+    if (this.state.selectedOption === "LatinJazzFans") {
+        
+        this.LatinJazzFans();
+    }
+
+    
+    if (this.state.selectedOption === "HouseHeads") {
+      this.HouseHeads();
+    }
+   
+    if (this.state.selectedOption === "FunkFans") {
+      this.FunkFans();
+    }
+    if (this.state.selectedOption === "JazzCats") {
+      this.JazzCats();
+    }
+  };
+
+  LatinJazzFans = i => {
+
+    let pushed = [];
+
+    for (i = 0; i < this.state.groups.LatinJazzFans.length; i++) {
+      pushed.push(this.state.groups.LatinJazzFans[i]);
+    }
+    this.setState({
+      items: [pushed[0], pushed[1], pushed[2]]
+    });
+
+    
+
+   
+  };
+
+  
+
+  HouseHeads = i => {
+ 
+    let pushed = [];
+
+    for (i = 0; i < this.state.groups.HouseHeads.length; i++) {
+      pushed.push(this.state.groups.HouseHeads[i]);
+    }
+    this.setState({
+      items: [pushed[0], pushed[1], pushed[2]]
+    });
+
+   
+  };
+
+  FunkFans = i => {
+   
+    let pushed = [];
+
+    for (i = 0; i < this.state.groups.FunkFans.length; i++) {
+      pushed.push(this.state.groups.FunkFans[i]);
+    }
+    this.setState({
+      items: [pushed[0], pushed[1], pushed[2]]
+    });
+
+  };
+
+
+  JazzCats = i => {
+    
+    let pushed = [];
+
+    for (i = 0; i < this.state.groups.JazzCats.length; i++) {
+      pushed.push(this.state.groups.JazzCats[i]);
+    }
+    this.setState({
+      items: [pushed[0], pushed[1], pushed[2]]
+    });
+   
+    
+  };
+
+
+  mapBrain = () => {
+    //passing group
+   
+    
+    let mapOne = this.state.items.map(elem => (
+        <ul>
+          {elem}
+
+          <button
+          className="buttonTwo"
+          onClick={() => {
+            this.handleSubmit(elem);
+            this.handleExperience(elem);
+            this.setState({ elem: elem });
+          }}
+        >
+          DELETE
+        </button>
+      
+       
+        </ul>
+      ));
+      return mapOne
+
+
+  }
+  handleChange = event => {
+  
+    this.setState({ selectedOption: event.target.value}, () =>
+      this.triggerGroup()
+    );
+  };
+  onChange = event => {
+    event.preventDefault();
+    this.setState({ text: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div />
+
+        <select
+          value={this.state.selectedOption}
+          className="Dropdown-menuTwo"
+          onChange={this.handleChange}
+        >
+          <option selected value="SelectAGroup">
+            Select A Music Group
+          </option>
+          <option value="FunkFans">Funk Fans</option>
+          <option value="JazzCats">Jazz Cats</option>
+          <option value="LatinJazzFans">Latin Jazz Fans</option>
+         
+          <option value="HouseHeads">House Heads</option>
+        </select>
+        <li className="NameListTwo">{this.mapBrain()}</li>
+      </div>
+    );
+  }
+}
+export default Users;
